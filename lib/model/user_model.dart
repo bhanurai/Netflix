f7fee066ca4bf69711ac76426950e1dd31ebffe6
Bhanu rai <bhanurai001@gmail.com>
2023-01-12 12:05:32 +0545

    init

---
+++ lib/model/user_model.dart
@@ -0,0 +1,48 @@
+import 'dart:convert';
+import 'package:cloud_firestore/cloud_firestore.dart';
+
+UserModel? userModelFromJson(String str) => UserModel.fromJson(json.decode(str));
+
+String userModelToJson(UserModel? data) => json.encode(data!.toJson());
+
+class UserModel {
+  UserModel({
+    this.userId,
+    this.fcmToken,
+    this.name,
+    this.username,
+    this.phone,
+  });
+
+  String? userId;
+  String? fcmToken;
+  String? name;
+  String? username;
+  String? phone;
+
+  factory UserModel.fromJson(Map<String, dynamic> json) => UserModel(
+    userId: json["user_id"],
+    fcmToken: json["fcm_token"],
+    name: json["name"],
+    username: json["username"],
+    phone: json["phone"],
+  );
+
+  factory UserModel.fromFirebaseSnapshot
+      (DocumentSnapshot<Map<String,dynamic>>json) => UserModel(
+    userId: json["user_id"],
+    fcmToken: json["fcm_token"],
+    name: json["name"],
+    username: json["username"],
+    phone: json["phone"],
+  );
+
+
+  Map<String, dynamic> toJson() => {
+    "user_id": userId,
+    "fcm_token": fcmToken,
+    "name": name,
+    "username": username,
+    "phone": phone,
+  };
+}
